4ced9581a43abc053f26e815cf93e0c65e43ebc4
Pavel Ilin <ilin.pa@gmail.com>
2013-05-30 19:24:49 +0200

    Install templates into user Library forlder
    Tested with XCode 4.6.2

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-cp -R sdk/Project\ Templates /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates
-cp -R sdk/File\ Templates /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates
+mkdir -p ~/Library/Developer/Xcode/Templates
+cp -R sdk/Project\ Templates ~/Library/Developer/Xcode/Templates/File\ Templates
+cp -R sdk/File\ Templates ~/Library/Developer/Xcode/Templates/Project\ Templates
+
 mkdir -p /Library/Frameworks/SkankySDK.framework
 cp -R include /Library/Frameworks/SkankySDK.framework/Headers
 cp -R resource /Library/Frameworks/SkankySDK.framework/Resources
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates/Kineme\ SkankySDK/
-rm -rf /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/Project\ Templates/Kineme\ SkankySDK/
+rm -rf ~/Library/Developer/Xcode/Templates/File\ Templates/Kineme\ SkankySDK/
+rm -rf ~/Library/Developer/Xcode/Templates/Project\ Templates/Kineme\ SkankySDK/
 rm -rf /Library/Frameworks/SkankySDK.framework/
